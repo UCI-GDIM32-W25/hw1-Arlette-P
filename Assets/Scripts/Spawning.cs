5c96c941556406ce7f1a39213ee4387d850cee29
Arlette-P <apoienar@uci.edu>
2025-01-12 23:18:50 -0800

    Player can now plant plants (capped at 5). Still need to work on UI

---
+++ Assets/Scripts/Spawning.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+    [SerializeField] private GameObject _plantPrefab;
+    [SerializeField] private Transform Spawn;
+    [SerializeField] private int _numSeeds = 5;
+    [SerializeField] private PlantCountUI _plantCountUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_numSeeds > 0)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(_plantPrefab, Spawn.position, Quaternion.identity);
+                _numSeeds--;
+            }
+        }
+        
+    }
+}
